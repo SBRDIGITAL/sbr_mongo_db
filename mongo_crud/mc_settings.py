4049f30352f958b3abcd5a4b71b2b1fd470d206e
Vasiliy <SBRDigital@yandex.ru>
2024-12-01 20:01:45 +0300

    update

--- mongo_crud/mc_settings.py
+++
@@ -1,9 +0,0 @@
-from typing import Literal
-
-
-class MongoCRUDHelper:
-    QueryModes = Literal['one', 'many']
-    
-class Settings:
-    URI = 'mongodb://localhost:27017/'
-    DB_NAME = 'test_db'
